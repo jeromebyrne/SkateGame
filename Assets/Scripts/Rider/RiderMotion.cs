81e33c08d2970e41bbb994102b51f4fe2a83bd24
jeromebyrne <jeromebyrne21@hotmail.com>
2024-06-26 23:08:37 -0700

    experimenting with SpriteShape

--- Assets/Scripts/Rider/RiderMotion.cs
+++ Assets/Scripts/Rider/RiderMotion.cs
@@ -4,17 +4,12 @@ public class RiderMotion : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _riderRigidBody = null;
 
-    private readonly Vector2 JUMP_FORCE = new Vector2(0.0f, 275.0f);
+    private readonly Vector2 BASE_JUMP_FORCE = new Vector2(0.0f, 150.0f);
     private readonly Vector2 PUSH_FORCE = new Vector2(1000.0f, 0.0f);
-    private const float MAX_VELOCITY_X = 120.0f;
+    private const float MAX_VELOCITY_X = 150.0f;
+    private const float JUMP_FORCE_MULTIPLIER = 0.6f;
+    private const float MAX_JUMP_FORCE = 300.0f; 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         ClampVelocityX();
@@ -27,7 +22,12 @@ public class RiderMotion : MonoBehaviour
 
     public void Jump()
     {
-        _riderRigidBody.AddForce(JUMP_FORCE, ForceMode2D.Impulse);
+        float xVelocity = Mathf.Abs(_riderRigidBody.velocity.x);
+        float proportionalJumpForceY = BASE_JUMP_FORCE.y + (xVelocity * JUMP_FORCE_MULTIPLIER);
+        proportionalJumpForceY = Mathf.Min(proportionalJumpForceY, MAX_JUMP_FORCE); // Clamp to max jump force
+        Vector2 proportionalJumpForce = new Vector2(0.0f, proportionalJumpForceY);
+
+        _riderRigidBody.AddForce(proportionalJumpForce, ForceMode2D.Impulse);
     }
 
     private void ClampVelocityX()
